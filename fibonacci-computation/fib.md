864cb4c9c19477e66fc2c65365c5836c3bf77c89
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-01-31 21:43:55 -0500

    python script for fibonnaci

---
+++ fibonacci-computation/fib.md
@@ -0,0 +1,4 @@
+# Fibonacci
+In this project we will compare the computational time taken by a recursive algorithm to determine the Fibonacci number of an integer n and the time taken by a bottom-up approach (using a loop)  to calculate the Fibonacci number of the same integer n.
+
+This code contains the same algorithms written in both C++ and Python (written in two just to see if theres any time difference) that computes the time (in seconds using the time library file that takes to determine Fibonacci (n) using FiboR and the time taken by FiboNR on the same input n. Both routines are tested using different values of n. 
---
+++ fibonacci-computation/fibonacci.py
@@ -0,0 +1,45 @@
+import time
+import random 
+# recursive fibonacci function
+def FiboR(value):
+    if value == 0 or value == 1:
+        return value
+    return (FiboR(value - 1) + FiboR(value - 2))
+
+
+def FiboNR(value) :
+    # first 2 values of fibonacci
+    Fib = [0,1]
+    # loop until value entered
+    for i in range(2, value + 1):
+        Fib.append(Fib[i-1] + Fib[i-2])
+    return Fib[value]
+
+
+# test fib - recusrive
+print('recursive fibonacci: ',  FiboR(10))
+print('non-recursive fibonacci: ',  FiboNR(10))
+
+
+#format outputs
+print ('Integer'.ljust(20),'FiboR (seconds)'.ljust(25), 'FiboNR (seconds)'.ljust(30), 'Fibo-value')
+print('-'.ljust(95, '-'))\
+
+#different values for testing
+values = [1,5,10, 15,20,25,30,35,40,45,55,60]
+
+for j in range(len(values)):
+
+    # time for recursive method
+    timeStart = time.time()
+    fibval = FiboR(values[j])
+    fibRtime = time.time() - timeStart
+
+    # time for non-recursive
+    timeStart = time.time()
+    fibval = FiboNR(values[j])
+    fibNRtime = time.time() - timeStart
+
+    #output values 
+    print (str(values[j]).ljust(15), str(round(fibRtime, 3)).center(24), ' '.ljust(11), str(fibNRtime).ljust(18), str(fibval).center(24))
+

a775accca97955d166e70141a746670069673adc
Dennis Krupitsky <d.krupitsky@gmail.com>
2020-03-09 21:20:50 -0400

    simply added random num generation

--- average-case-insertion-sort/average-case.py
+++ average-case-insertion-sort/average-case.py
@@ -3,10 +3,28 @@
 # Project #3 Average Case - Insertion Sort
 # Date: 3/5/20
 
-def Insertion(A, n):
+# import lib
+import random
+
+# variables used
+A = []
+n = 100
+steps = 0
+bound = 50
+
+# insertion sort method
+def Insertion_Mod(A, n):
+    # bring in our global var for steps
+    global steps
+
     for i in range(1,n):
         j = i
         while A[j] < A[j-1]:
+            # increment steps based on comparisons
+            steps += 1
+            # print('comparing ', A[j], ' with ', A[j-1])
+
+            #sort the values
             temp = A[j]
             A[j] = A[j-1]
             A[j-1]=temp
@@ -14,7 +32,13 @@ def Insertion(A, n):
     return A
 
 
+    
 
-A=[0,1,4,8,2,9,3,5]
-print(Insertion(A,len(A)))
+
+A = random.sample(range(-bound, bound), n)
+
+
+print(A)
+# print(Insertion(Apru,len(A)))
+# print(steps)
 
